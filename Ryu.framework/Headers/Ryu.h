03243c47183ed6888a2f8cfe0c2d962e9a594e68
Wyatt Mufson <wyatt@ryucoin.com>
2020-04-06 00:05:29 -0700

    New framework

--- Ryu.framework/Headers/Ryu.h
+++ Ryu.framework/Headers/Ryu.h
@@ -16,5 +16,3 @@ FOUNDATION_EXPORT double RyuVersionNumber;
 FOUNDATION_EXPORT const unsigned char RyuVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Ryu/PublicHeader.h>
-
-
